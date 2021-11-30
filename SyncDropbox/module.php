73f8a347bdee72f87e3f4bc417af31d727c817c4
Michael Maroszek <maroszek@gmx.net>
2021-11-30 14:35:33 +0100

    change valid utf-8 detection

--- SyncDropbox/module.php
+++ SyncDropbox/module.php
@@ -321,7 +321,17 @@ declare(strict_types=1);
             }
 
             //Any non UTF-8 filename will break everything. Therefore we need to filter them
-            if (!mb_check_encoding($file, 'UTF-8')) {
+            //See: https://stackoverflow.com/a/1523574/10288655 (Regex seems to be faster than mb_check_encoding)
+            if (!preg_match('%^(?:
+              [\x09\x0A\x0D\x20-\x7E]            # ASCII
+            | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+            | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+            | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+            | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+            | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+            | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+            )*$%xs', $file)) {
                 return true;
             }
 
